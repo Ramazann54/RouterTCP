731fc0fd4fa4ed651eea1e9f63a491605abef388
Kiselev Daniil <kiselev.d.e@bk.ru>
2023-10-19 10:16:08 +0600

    make_adding_routes_activity

---
+++ frontend/android/app/src/main/java/com/example/routerTCP/view/main/add_and_edit_routes/AddingRoutesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.routerTCP.view.main.add_and_edit_routes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.routerTCP.R
+
+class AddingRoutesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adding_routes)
+    }
+}
