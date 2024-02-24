2b0544cac926b3921504574002918cc1393089ed
Kiselev Daniil <kiselev.d.e@bk.ru>
2024-02-24 19:01:04 +0600

    try_to_add_edit

--- frontend/android/app/src/main/java/com/example/routerTCP/presentation/abstractions/IDeleteDialogPresenter.kt
+++ frontend/android/app/src/main/java/com/example/routerTCP/presentation/abstractions/IDeleteDialogPresenter.kt
@@ -1,6 +1,5 @@
 package com.example.routerTCP.presentation.abstractions
 
-import com.example.routerTCP.presentation.abstractions.IDialogPresenter
 import com.example.routerTCP.view.abstractions.IDeleteDialogView
 
 interface IDeleteDialogPresenter : IDialogPresenter<IDeleteDialogView>
